3d633b2c955b880526b4a17d1dfd6d162441dfc5
AnniesTech <anita.rocha0505@gmail.com>
2022-05-25 20:08:15 -0500

    task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,12 +9,11 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
     states = storage.all("State").values()
-    states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
